be37154ca672f9bab9909cfd9152f75eb3ef75d3
Iordan Robert <robert.iordan-extern@renault.com>
2021-03-07 19:17:04 +0200

    fix mobile navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  showMobileMenu = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
   }
 
 }
