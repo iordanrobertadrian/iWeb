ff171e914a61cf395752a8b58df0da7bd563ba44
Robert <iradev.design@gmail.com>
2021-03-10 20:34:45 +0200

    integrate firestore

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from 'rxjs';
+interface Review {
+  description: string;
+  reviewedBy: string;
+  imageURL: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  reviews: Observable<Review[]>
+  constructor(private afs: AngularFirestore) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.reviews = this.afs.collection<Review>('Reviews').valueChanges();
   }
 
 }
