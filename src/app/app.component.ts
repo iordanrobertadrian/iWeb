2b34c41d5de26ebf732e16c7babda9d89ef5fc58
Iordan Robert <robert.iordan-extern@renault.com>
2021-04-01 21:24:28 +0300

    add overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'iWeb';
+  ngOnInit(): void {
+    setTimeout(
+      () => {
+        document.getElementById("overlay").style.display = "block";
+      }, 5000
+    )
+  }
+  off() {
+    document.getElementById("overlay").style.display = "none";
+  }
 }
