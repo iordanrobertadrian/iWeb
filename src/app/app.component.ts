517c47c5ce701d876f4e06ca650ddc67d4238114
Iordan Robert <robert.iordan-extern@renault.com>
2021-05-12 17:45:52 +0300

    fix metatags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,17 +17,15 @@ export class AppComponent  implements OnInit{
 
   ngOnInit() {
     this.metaTagService.addTags([
-      { name: 'keywords', content: 'Creare site, Creare magazin online, Creare site web, Creare site web prezentare, Creare site prezentare, Site, Site web' },
+      { name: 'keywords', content: 'Creare site,Creare magazin online,Creare site web,Creare site web prezentare,Creare site prezentare,Site,Site web' },
       { name: 'robots', content: 'index, follow' },
       { name: 'author', content: 'iWeb Solution' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'date', content: '2018-08-01', scheme: 'YYYY-MM-DD' },
+      { name: 'description', content: 'Servicii profesionale in ceea ce priveste crearea de magazine online si site-uri web pentru prezentarea afacerii dumneavoastra.' },
       { charset: 'UTF-8' }
     ]);
     this.titleService.setTitle(this.title);
-    this.metaTagService.updateTag(
-      { name: 'description', content: 'Compania iWeb ofera servicii profesionale in domeniul IT, cum ar fi dezvoltarea web, concept unic in ceea ce priveste design-ul web (UX / UI), crearea de logo-uri, site-uri pentru prezentare, magazine online si nu in ultimul rand ne ocupam de tot ce tine de partea de SEO.' }
-    );
     this.canonicalService.setCanonicalURL();
   }
 }
